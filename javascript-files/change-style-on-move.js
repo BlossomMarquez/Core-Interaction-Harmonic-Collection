66ca9f12e555b7aabea2121797c2ecb841f3a7c6
BlossomMarquez <marqb058@newschool.edu>
2024-10-03 16:04:05 +0000

    lots of stuff aghhhh

---
+++ javascript-files/change-style-on-move.js
@@ -0,0 +1,15 @@
+let isStyle1 = true; // Flag to track current style
+
+// Function to switch CSS files
+function switchStyle() {
+    const stylesheet = document.getElementById('theme-stylesheet');
+    if (isStyle1) {
+        stylesheet.setAttribute('href', '../CSS/style2.css');
+    } else {
+        stylesheet.setAttribute('href', '../CSS/style1.css');
+    }
+    isStyle1 = !isStyle1; // Toggle the style flag
+}
+
+// Add event listener for mouse click
+document.addEventListener('mouseout', switchStyle);
